994f67736e6ee5c6e28bab63b381b929df57ac5a
Pavan699 <pavannakate03@gmail.com>
2021-09-07 05:14:44 -0700

    Calculate length of One Line

---
+++ Line_Comparison_Refactor/Length.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Line_Comparison_Refactor
+{
+    public class Length
+    {
+        public static void check()
+        {
+            int x1, x2, y1, y2;
+            double length_One;
+            x1 = 4;
+            y1 = 1;
+            x2 = 1;
+            y2 = 3;
+
+            int resultone = ((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            length_One = Math.Abs((0.5) * resultone);
+            Console.WriteLine("Length Of the coordinates ({0},{1}) and ({2},{3}) is : {4}", x1, y1, x2, y2, length_One);
+        }
+        
+    }
+}
--- Line_Comparison_Refactor/Program.cs
+++ Line_Comparison_Refactor/Program.cs
@@ -2,11 +2,12 @@
 
 namespace Line_Comparison_Refactor
 {
-    class Program
+    class Program : Length
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Line Comparison With OOPs of C-Sharp");
+            check();
         }
     }
 }
