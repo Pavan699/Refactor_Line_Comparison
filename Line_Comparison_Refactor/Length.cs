21efbd652de11d40cbe3caf0c5980168edc9069e
Pavan699 <pavannakate03@gmail.com>
2021-09-07 05:44:46 -0700

    Adding Comments To the Code

--- Line_Comparison_Refactor/Length.cs
+++ Line_Comparison_Refactor/Length.cs
@@ -6,9 +6,11 @@ namespace Line_Comparison_Refactor
 {
     public class Length
     {
+        //check method to calculate lengths of two lines
         public static void check()
         {
             Console.WriteLine("Welcome To Line Comparison Computation Program");
+            //Declaring variables....
             int x1, x2, y1, y2, x3, x4, y3, y4;
             double length_One, length_Two;
             x1 = 4; x3 = 4;
@@ -16,23 +18,27 @@ namespace Line_Comparison_Refactor
             x2 = 1; x4 = 1;
             y2 = 3; y4 = 3; 
 
+            //Logic
             int resultone = ((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            length_One = Math.Abs((0.5) * resultone);
+            length_One = Math.Abs((0.5) * resultone);//Math.Abs() used to convert the negative to positive number
             Console.WriteLine("Length Of the coordinates ({0},{1}) and ({2},{3}) is : {4}", x1, y1, x2, y2, length_One);
 
             int resulttwo = ((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
-            length_Two = Math.Abs((0.5) * resulttwo);
+            length_Two = Math.Abs((0.5) * resulttwo);//You can use Math.Sqrt() method
             Console.WriteLine("Length Of the coordinates ({0},{1}) and ({2},{3}) is : {4}", x3, y3, x4, y4, length_Two);
+            //Created Object of Length class to call equal() method
             Length l = new Length();
-            l.equal(length_One, length_Two);
+            l.equal(length_One, length_Two);//calling equal method with help of referance
+            //Calling compare() Method (It is static method in same class)
             comapre(length_One, length_Two);
             
         }
+        //Checking Equals() method
         public void equal(double length_One,double length_Two)
         {
             Console.WriteLine("Equals() Method : " + length_One.Equals(length_Two));
         }
-
+        //Checking CompareTo() method
         public static void comapre(double length_One, double length_Two)
         {
             Console.WriteLine("CompareTo() Method : " + length_One.CompareTo(length_Two));
--- Line_Comparison_Refactor/Program.cs
+++ Line_Comparison_Refactor/Program.cs
@@ -2,11 +2,13 @@
 
 namespace Line_Comparison_Refactor
 {
+    //Used Inheritance concept to call the methods in Length
     class Program : Length
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Line Comparison With OOPs of C-Sharp");
+            //calling the static check() method from Length Class
             check();           
         }
     }
